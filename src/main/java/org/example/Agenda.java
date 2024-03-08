60b9ee3ea515c6fafe65a3f9faffdd83e3f9136d
MarioCasBer <m.casado86@gmail.com>
2024-03-08 23:50:26 +0100

    Añadidos comentarios Javadoc a las clases Agenda y Contacto.

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Esta clase crea una lista de contactos y las almacena para su posterior acceso y uso.
+ * @author mario
+ */
 public class Agenda {
     private List<Contacto> contacts; // Lista de Contacto
 
+    /**
+     * Este método es un constructor de la clase agenda por defecto.
+     */
     public Agenda() {
         this.contacts = new ArrayList<>();
     }
 
+    /**
+     * Método para añadir contactos nuevos a la agenda.
+     * @param name
+     * @param phone
+     */
     public void addContact(String name, String phone) {
         boolean exists = false;
         for (Contacto c : contacts) {
@@ -27,6 +39,10 @@ public class Agenda {
         }
     }
 
+    /**
+     * Método para eliminar contactos de la agenda.
+     * @param name
+     */
     public void removeContact(String name) {
         Iterator<Contacto> it = contacts.iterator();
 
@@ -39,6 +55,12 @@ public class Agenda {
         }
     }
 
+    /**
+     * Método para modificar un contacto de la agenda.
+     * @param name
+     * @param oldPhone
+     * @param newPhone
+     */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
         for (Contacto c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
@@ -53,10 +75,18 @@ public class Agenda {
         }
     }
 
+    /**
+     * Método para obtener un contacto de la agenda.
+     * @return
+     */
     public List<Contacto> getContacts() {
         return this.contacts;
     }
 
+    /**
+     * Método para establecer un contacto.
+     * @param contacts
+     */
     public void setContacts(List<Contacto> contacts) {
         this.contacts = contacts;
     }
--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -3,20 +3,37 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta clase permite crear contactos y almacenar sus datos. Estos contactos podrán almacenarse en una lista o agenda.
+ * @author mario
+ */
 class Contacto {
     private String name;
     private List<String> phones;
 
+    /**
+     * Método constructor por parámetros.
+     * @param name
+     * @param phone
+     */
     public Contacto(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
     }
 
+    /**
+     *Método para obtener el nombre almacenado en un contacto.
+     * @return
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Método para obtener una lista con los numeros de teléfono de un contacto.
+     * @return
+     */
     public List<String> getPhones() {
         return this.phones;
     }
